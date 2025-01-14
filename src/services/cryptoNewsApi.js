2e6083b916d659b09329dae0c3fd5f57321873b7
Adya Tiwari <103478795+adya14@users.noreply.github.com>
2025-01-15 00:10:15 +0530

    Update cryptoNewsApi.js

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
     'X-BingApis-SDK': 'true',
-    'X-RapidAPI-Key': 'f5d96624c6mshc0d9c460c65ebdcp1d29b4jsn73a5ae29a5ff',
+    'X-RapidAPI-Key': 'your-api-key',
     'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
 };
 
